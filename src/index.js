bd07632907fd6253cb59a923350932405a3f458a
Tetiana Siestova <siestova.t@gmail.com>
2023-01-25 19:42:37 +0200

    validation fixed

--- src/index.js
+++ src/index.js
@@ -6,64 +6,58 @@ class App {
     this.textContainer = document.querySelector(".form__message");
     this.button = document.querySelector(".btn__request");
     this.emailInput = document.querySelector("#email");
-    this.emailPattern = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
   }
 
   addEventListeners() {
     this.form.addEventListener("submit", (e) => this.handleSubmit(e));
 
-    // this.emailInput.addEventListener("blur", (e) => this.handleChangeInput(e));
-
-    this.emailInput.addEventListener("blur", (e) => this.handleInput(e));
-    this.emailInput.addEventListener("focus", (e) => this.handleInput(e));
-    this.emailInput.addEventListener("keypress", (e) => this.handleInput(e));
+    this.emailInput.addEventListener("blur", (e) => this.handleBlur(e));
+    this.emailInput.addEventListener("focus", (e) => this.handleFocus(e));
+    this.emailInput.addEventListener("keypress", (e) => this.handleKeypress(e));
   }
 
-  handleFocus(e) {
-    if (e.key === "Enter") {
+  isValidEmail(value) {
+    const pattern = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+    const result = !value.length || !value.match(pattern);
+
+    return !result;
+  }
+
+  handleBlur(e) {
+    const value = e.target.value;
+    const validation = this.isValidEmail(value);
+
+    if (!validation) {
       this.textContainer.textContent = "Oops! Please add your email";
       this.button.disabled = true;
     }
+  }
 
+  handleFocus() {
     this.textContainer.textContent = "";
     this.button.disabled = false;
   }
 
-  handleInput(e) {
-    const eventType = e.type;
-    const inputValue = e.target.value;
-    const condition1 =
-      !inputValue.length || !inputValue.match(this.emailPattern);
-
-    const condition2 = condition1 && e.key === "Enter";
-
-    if ((eventType === "blur" && condition1) || (condition1 && condition2)) {
+  handleKeypress(e) {
+    const value = e.target.value;
+    const validation = this.isValidEmail(value);
+    if (e.key === "Enter" && !validation) {
       this.textContainer.textContent = "Oops! Please add your email";
       this.button.disabled = true;
-    }
-
-    if (e.key !== "Enter" && eventType !== "blur") {
-      this.textContainer.textContent = "";
-      this.button.disabled = false;
-    }
-
-    if (eventType === "focus") {
-      this.textContainer.textContent = "";
-      this.button.disabled = false;
+      this.emailInput.blur();
     }
   }
 
   handleSubmit(e) {
     e.preventDefault();
-
     const form = e.target;
-
     const userEmail = form["user-email"].value;
+    const validation = this.isValidEmail(userEmail);
+    console.log(userEmail);
 
-    // if (!userEmail.length || !userEmail.match(this.emailPattern)) {
-    //   this.textContainer.textContent = "Oops! Please add your email";
-    //   return;
-    // }
+    if (!validation) {
+      return;
+    }
 
     this.button.disabled = true;
     fetch("https://jsonplaceholder.typicode.com/posts", {
@@ -76,7 +70,6 @@ class App {
       }),
     })
       .then((data) => {
-        //this.textContainer.textContent = "";
         this.form.reset();
         this.button.disabled = false;
       })
