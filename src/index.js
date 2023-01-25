e9d1b4b8fbd24f40aad209b908858eb5378fd274
Tetiana Siestova <siestova.t@gmail.com>
2023-01-25 14:56:19 +0200

    email input

--- src/index.js
+++ src/index.js
@@ -4,10 +4,53 @@ class App {
   constructor() {
     this.form = document.querySelector("#form");
     this.textContainer = document.querySelector(".form__message");
+    this.button = document.querySelector(".btn__request");
+    this.emailInput = document.querySelector("#email");
+    this.emailPattern = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
   }
 
   addEventListeners() {
     this.form.addEventListener("submit", (e) => this.handleSubmit(e));
+
+    // this.emailInput.addEventListener("blur", (e) => this.handleChangeInput(e));
+
+    this.emailInput.addEventListener("blur", (e) => this.handleInput(e));
+    this.emailInput.addEventListener("focus", (e) => this.handleInput(e));
+    this.emailInput.addEventListener("keypress", (e) => this.handleInput(e));
+  }
+
+  handleFocus(e) {
+    if (e.key === "Enter") {
+      this.textContainer.textContent = "Oops! Please add your email";
+      this.button.disabled = true;
+    }
+
+    this.textContainer.textContent = "";
+    this.button.disabled = false;
+  }
+
+  handleInput(e) {
+    const eventType = e.type;
+    const inputValue = e.target.value;
+    const condition1 =
+      !inputValue.length || !inputValue.match(this.emailPattern);
+
+    const condition2 = condition1 && e.key === "Enter";
+
+    if ((eventType === "blur" && condition1) || (condition1 && condition2)) {
+      this.textContainer.textContent = "Oops! Please add your email";
+      this.button.disabled = true;
+    }
+
+    if (e.key !== "Enter" && eventType !== "blur") {
+      this.textContainer.textContent = "";
+      this.button.disabled = false;
+    }
+
+    if (eventType === "focus") {
+      this.textContainer.textContent = "";
+      this.button.disabled = false;
+    }
   }
 
   handleSubmit(e) {
@@ -17,30 +60,30 @@ class App {
 
     const userEmail = form["user-email"].value;
 
-    const pattern = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+    // if (!userEmail.length || !userEmail.match(this.emailPattern)) {
+    //   this.textContainer.textContent = "Oops! Please add your email";
+    //   return;
+    // }
 
-    if (!userEmail.length || !userEmail.match(pattern)) {
-      this.textContainer.textContent = "Oops! Please add your email";
-    } else {
-      fetch("https://jsonplaceholder.typicode.com/posts", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          rating: userEmail,
-        }),
+    this.button.disabled = true;
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: userEmail,
+      }),
+    })
+      .then((data) => {
+        //this.textContainer.textContent = "";
+        this.form.reset();
+        this.button.disabled = false;
       })
-        .then((data) => {
-          console.log(userEmail);
-          this.form.reset();
-          this.textContainer.textContent = "";
-        })
 
-        .catch((e) => {
-          console.log(e);
-        });
-    }
+      .catch((e) => {
+        console.log(e);
+      });
   }
 
   init() {
